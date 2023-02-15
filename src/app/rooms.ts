3d8538a1b1509c608a67681b9a458c77006bda57
Karissa Phillips <phillips.karissa00@gmail.com>
2023-02-15 12:02:26 -0500

    data update

--- src/app/rooms.ts
+++ src/app/rooms.ts
@@ -21,51 +21,17 @@ interface Review {
 export const rooms: RoomsList[] = [
   {
     roomId: 1,
-    name: 'Room 1',
+    name: 'Golden Hour',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      'This room features a queen sized bed facing the sunset. Bask in the warm glow as you sink into our softest pillows.',
     price: 175,
     photo: 'https://posterstore.com/images/zoom/v4-kg-102.jpg',
     rating: 4.5,
     checkinTime: 3,
     checkoutTime: 11,
-    amenities: ['A/C', 'TV', 'iron', 'free parking', 'continental breakfast'],
-    reviews: [
-      {
-        id: 1,
-        name: 'Sara',
-        rating: 5,
-        message: 'This room restored my soul and refreshed my energy.',
-      },
-      {
-        id: 2,
-        name: 'Nicole',
-        rating: 4,
-        message: 'Would definitely stay here again.',
-      },
-      {
-        id: 3,
-        name: 'John',
-        rating: 4.5,
-        message: 'The perfect getaway from a busy life.',
-      },
-    ],
-  },
-  {
-    roomId: 2,
-    name: 'Room 2',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    price: 400,
-    photo:
-      'http://cdn.home-designing.com/wp-content/uploads/2021/10/earthy-bedroom-decor.jpg',
-    rating: 4.8,
-    checkinTime: 3,
-    checkoutTime: 11,
     amenities: [
       'A/C',
       'TV',
-      'iron',
       'free parking',
       'continental breakfast',
       'private balcony',
@@ -92,23 +58,62 @@ export const rooms: RoomsList[] = [
     ],
   },
   {
-    roomId: 3,
-    name: 'Room 3',
+    roomId: 2,
+    name: 'Gaia Suite',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    price: 250,
+      'Let us help calm your senses in our most grounded room. This suite features a king sized bed with our finest linens. Reconnect with the earth while enjoying our complimentary clay masks.',
+    price: 400,
     photo:
-      'https://cdn.decoist.com/wp-content/uploads/2021/03/decoist012-91604.jpg',
-    rating: 3.7,
+      'http://cdn.home-designing.com/wp-content/uploads/2021/10/earthy-bedroom-decor.jpg',
+    rating: 4.8,
     checkinTime: 3,
     checkoutTime: 11,
     amenities: [
       'A/C',
       'TV',
-      'iron',
+      'free parking',
+      'continental breakfast',
+      'hot stone massage',
+    ],
+    reviews: [
+      {
+        id: 1,
+        name: 'Sara',
+        rating: 5,
+        message: 'This room restored my soul and refreshed my energy.',
+      },
+      {
+        id: 2,
+        name: 'Nicole',
+        rating: 4,
+        message: 'Would definitely stay here again.',
+      },
+      {
+        id: 3,
+        name: 'John',
+        rating: 4.5,
+        message: 'The perfect getaway from a busy life.',
+      },
+    ],
+  },
+  {
+    roomId: 3,
+    name: 'Zen Garden',
+    description:
+      'Take a deep breath of fresh air in our plant filled oasis. This room includes a double bed and a luxurous waterfall shower. Enjoy our complimentary teas and yoga class in this zen experience.',
+    price: 250,
+    photo:
+      'https://cdn.decoist.com/wp-content/uploads/2021/03/decoist012-91604.jpg',
+    rating: 3.9,
+    checkinTime: 3,
+    checkoutTime: 11,
+    amenities: [
+      'A/C',
+      'TV',
       'free parking',
       'continental breakfast',
       'waterfall shower',
+      'yoga class',
     ],
     reviews: [
       {
