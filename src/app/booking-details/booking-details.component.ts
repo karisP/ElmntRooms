315fb297388e114a003e7b5a9d45b2d058192396
Karissa Phillips <phillips.karissa00@gmail.com>
2023-02-16 12:34:03 -0500

    calculations for the prices

--- src/app/booking-details/booking-details.component.ts
+++ src/app/booking-details/booking-details.component.ts
@@ -9,6 +9,11 @@ import { rooms, RoomsList } from '../rooms';
 })
 export class BookingDetailsComponent {
   room: RoomsList | undefined;
+  roomCost: number | undefined;
+  numberOfNights: number | undefined;
+  taxes: number | undefined;
+  fees: number | undefined;
+  totalPrice: number | undefined;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -16,5 +21,35 @@ export class BookingDetailsComponent {
     const routeParams = this.route.snapshot.paramMap;
     const roomIdFromRoute = Number(routeParams.get('id'));
     this.room = rooms.find((room) => room.roomId === roomIdFromRoute);
+    // replace later with actual dates
+    this.numberOfNights = 4;
+    this.roomCostCalculator();
+    this.taxesCalculator();
+    this.feesCalculator();
+    this.totalPriceCalculator();
+  }
+
+  roomCostCalculator() {
+    if (this.room && this.numberOfNights) {
+      this.roomCost = this.room?.price * this.numberOfNights;
+    }
+  }
+
+  taxesCalculator() {
+    if (this.roomCost) {
+      this.taxes = this.roomCost * 0.06;
+    }
+  }
+
+  feesCalculator() {
+    if (this.numberOfNights) {
+      this.fees = 10 * this.numberOfNights;
+    }
+  }
+
+  totalPriceCalculator() {
+    if (this.roomCost && this.taxes && this.fees) {
+      this.totalPrice = this.roomCost + this.taxes + this.fees;
+    }
   }
 }
---
+++ src/app/booking-details/booking-details.ts
@@ -0,0 +1,6 @@
+export interface Booking {
+  id: number;
+  roomId: number;
+  checkInDate: Date;
+  checkoutDate: Date;
+}
